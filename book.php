0a9d25ea8d3694e8d834130c31f6314ddcf098a2
emil <emil@htmlbox.net>
2016-10-17 18:09:02 +0300

    PHP 5.3 compat; specify response content type to JSON

--- book.php
+++ book.php
@@ -4,13 +4,13 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
 	$nights      = $_POST['nights'];
 	$price       = $_POST['price'];
 	$destination = $_POST['destination'];
-	$response    = [
+	$response    = array(
 		'nights'       => $nights,
 		'price'        => (int) $price * (int) $nights,
 		'destination'  => $destination,
 		'confirmation' => md5(time()),
-	];
-
+	);
+	header('Content-Type: application/json; charset=utf-8');
 	echo json_encode($response);
 } else {
 	echo 'Method not allowed.';
