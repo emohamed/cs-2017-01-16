04e9dcd13a64c8213fd51e2d86e2a2535d388ce8
emil <emil@htmlbox.net>
2016-10-17 17:35:41 +0300

    Initial Commit

---
+++ js/main.js
@@ -0,0 +1,7 @@
+$(document).ready(function() {
+	// - Използвайте AJAX, за да изпратите съдържанието на формата до `book.php` - http://learn.jquery.com/ajax/key-concepts/#get-vs-post, http://learn.jquery.com/ajax/ajax-and-forms/.
+	// - Покажете следното съобщение `Trip to {destination} at ${price} for {nights} nights. Your confirmation code: {data.confirmation}.`, като
+	//   заместите `{}` с резултата, който е върнат след обработката на информацията.
+	//
+	// Демо: https://s3-eu-west-1.amazonaws.com/codecraft-production/uploads/05af82d4340a14b88bb8122f93577b7c/book-a-trip.webm
+});
