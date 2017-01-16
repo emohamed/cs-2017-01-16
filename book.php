926899d59c81983cb330503799c2a316d6e8fa4b
Emil Mohamed <Emil Mohamed>
2017-01-16 16:34:34 +0200

    State from the end of the coding session

--- book.php
+++ book.php
@@ -1,9 +1,11 @@
 <?php
-
+sleep(1);
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
 	$nights      = $_POST['nights'];
 	$price       = $_POST['price'];
 	$destination = $_POST['destination'];
+	$num_children = $_POST['num_children'];
+
 	$response    = array(
 		'nights'       => $nights,
 		'price'        => (int) $price * (int) $nights,
--- js/main.js
+++ js/main.js
@@ -1,3 +1,32 @@
 $(document).ready(function() {
-	
+	$('form').on('submit', function (e) {
+		e.preventDefault();
+		var $form = $(this);
+
+		var destination = $('#destination').val();
+		var nights = $('#nights').val();
+
+		$form.addClass('loading');
+
+		$.ajax({
+			type: this.method,
+			url: this.action,
+			data: $form.serializeArray()
+		})
+		.always(function () {
+			$form.removeClass('loading');
+		})
+		.done(function (response) {
+			var message = 'Trip to ' + destination + ' at ' + response.price + ' for ' + nights + 
+				' nights. Your confirmation code: ' + response.confirmation + '.';
+
+			$('.message-success').html(message);
+
+			$form.addClass('success').trigger('reset');
+		})
+		.fail(function () {
+			alert("Something went wrong. Please try again later. ");
+		});
+
+	});
 });
